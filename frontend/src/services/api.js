c51c99f48d302944df0efaf1b46f3131ab06886c
Ramom Andrade Medeiros Da Silva <monmedeiros10@hotmail.com>
2021-05-17 20:16:38 +0000

    commit Final

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 //const api = axios.create({baseURL:'http://localhost:3001'})
 
-const api = axios.create({baseURL:'https://3001-amaranth-carp-fwzrzbve.ws-us04.gitpod.io/'})
+const api = axios.create({baseURL:'https://3001-gold-prawn-0hqbc004.ws-us04.gitpod.io/'})
 
 export default api;
