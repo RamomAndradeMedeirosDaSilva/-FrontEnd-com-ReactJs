38a3f5b0468d22c2d1203a5d4eca0ff51d94b3cd
Ramom Andrade Medeiros Da Silva <monmedeiros10@hotmail.com>
2021-05-17 14:18:11 +0000

    Commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({baseURL:'http://localhost:3001'})
+//const api = axios.create({baseURL:'http://localhost:3001'})
+
+const api = axios.create({baseURL:'https://3001-moccasin-tyrannosaurus-camln5u6.ws-us04.gitpod.io/'})
 
 export default api;
