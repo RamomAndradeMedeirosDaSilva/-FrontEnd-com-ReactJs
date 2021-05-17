1bb25ab0e7df944c587398e461cf334e51107503
MrCl0ck <levipedreira27@gmail.com>
2021-05-17 19:15:49 +0000

    c1

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 //const api = axios.create({baseURL:'http://localhost:3001'})
 
-const api = axios.create({baseURL:'https://3001-moccasin-tyrannosaurus-camln5u6.ws-us04.gitpod.io/'})
+const api = axios.create({baseURL:'https://3001-amaranth-carp-fwzrzbve.ws-us04.gitpod.io/'})
 
 export default api;
